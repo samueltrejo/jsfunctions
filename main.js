2173bbba9f4c37be0b225740df566e87bf019200
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-02 11:07:23 -0600

    namPrinter function works

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("test");
+//Zoe Ames
+//Britney SPears
+
+// const firstName = 'Zoe';
+// const lastName = 'Ames';
+// console.log(firstName + ' ' + lastName)
+// console.log(`${firstName} ${lastName}`);
+
+//const firstName = 'Britney';
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter('Zoe', 'Ames');
+namePrinter('John', 'Doe');
